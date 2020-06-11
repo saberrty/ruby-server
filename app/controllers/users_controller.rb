ad643d37f31526fad29fe0a06d8f93f66b9a356a
徐恒一 <920301419@qq.com>
2020-06-11 15:35:05 +0800

    make changes for routes && test it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+
+  def create_user
+    username = params['username']
+    password = params['password']
+    if username && password
+      User.create({ password: password, username: username })
+      render json: { users: User.find_by_username(username) }
+    else
+      render json: { mother: "fucker" }
+    end
+  end
+
+  def delete_user
+    username = params['username']
+    if username
+      User.find_by_username(username).delete
+      render json: { delete: "success" }
+    else
+      render json: { delete: "failed" }
+    end
+  end
+
+  def users_list
+    render json: { users: User.all }
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   get 'users/new'
   root 'welcome#index'
 
-  resource :users
+  resources :users, only: %i[] do
+    collection do
+      post 'create_user'
+      get 'users_list'
+      post 'delete_user'
+    end
+  end
 end
